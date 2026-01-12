f302b7e46b5c905f8f7743c97ab5b5a2a5f20890
Om Salve <realomsalve@gmail.com>
2026-01-13 01:16:38 +0530

    Add all

--- app/Components/Misc/AnimatedMenu.tsx
+++ app/Components/Misc/AnimatedMenu.tsx
@@ -96,7 +96,7 @@ export default function AnimatedMenu({ isOpen, onClose }: AnimatedMenuProps) {
 const MENU_ITEMS = [
   { label: 'Home', href: '/' },
   { label: 'About Us', href: '#aboutus' },
-  { label: 'Read EPSOC Quarterly', href: '/editorial' },
+  { label: 'Read the Editorial', href: '/archive' },
   { label: 'Events', href: '/events' },
   { label: 'Contact', href: '/CTA' },
 
